c46c02b78940c80c5745ad6531bb64fdcedcefa1
at14DAM2 <alvaro.lopez40@educa.madrid.org>
2023-10-20 16:49:23 +0200

    Interfaz

--- app/src/main/java/com/example/ejercicios_android/ut03/u3a3aSeleccion.java
+++ app/src/main/java/com/example/ejercicios_android/ut03/u3a3aSeleccion.java
@@ -3,14 +3,37 @@ package com.example.ejercicios_android.ut03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.ejercicios_android.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class u3a3aSeleccion extends AppCompatActivity {
 
+
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_u3a3a_seleccion);
+
+        spinner = findViewById(R.id.u3a3aSpinner);
+        List<String> spinnerArray =  new ArrayList<>();
+        spinnerArray.add("Item1");
+        spinnerArray.add("item2");
+        spinnerArray.add("item3");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.u3a3aSpinner);
+        sItems.setAdapter(adapter);
+
     }
+
+
 }
