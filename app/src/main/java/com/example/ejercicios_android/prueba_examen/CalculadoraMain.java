6abe99fd87c8550e067d666a7f409e8e927b3a35
at14DAM2 <alvaro.lopez40@educa.madrid.org>
2023-11-15 16:55:21 +0100

    a

---
+++ app/src/main/java/com/example/ejercicios_android/prueba_examen/CalculadoraMain.java
@@ -0,0 +1,16 @@
+package com.example.ejercicios_android.prueba_examen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ejercicios_android.R;
+
+public class CalculadoraMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora_main);
+    }
+}
